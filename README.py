1af9a02d9524158b4c2548bbf4efbc910c5dd46c
fantasystarlight <33779342+fantasystarlight@users.noreply.github.com>
2017-11-21 20:45:07 +0800

    Update README.py

--- README.py
+++ README.py
@@ -2,10 +2,11 @@
 just another respository
 
 import numpy
+import copy
 
 A=[[9,3,7],[6,5,2],[8,4,1]]
 B=[[1,2,3],[4,5,6],[7,8,9]]
-import copy
+
 def entropy(X):
     c=[]
     for i in range(len(X)):
@@ -18,9 +19,7 @@ def transfer(X):
     x = []
     for i in range(12):
         X = copy.deepcopy(X)
-        x.append(X)
-    
-        
+        x.append(X)      
     x[0][0][0], x[0][0][1] = x[0][0][1], x[0][0][0]
     x[1][0][1], x[1][0][2] = x[1][0][2], x[1][0][1]
     x[2][1][0], x[2][1][1] = x[2][1][1], x[2][1][0]
@@ -33,11 +32,9 @@ def transfer(X):
     x[9][1][1], x[9][2][1] = x[9][2][1], x[9][1][1]
     x[10][0][2], x[10][1][2] = x[10][1][2], x[10][0][2]
     x[11][1][2], x[11][2][2] = x[11][2][2], x[11][1][2]
-
     y = 1000000
     for k in range(len(x)):
-        c = entropy(x[k])
-        
+        c = entropy(x[k])       
         if y > c:
             y = c
             n = k
